1e09425145c72d8abe30af4f29b5d8bd173e0baf
Jordan Jackson <treyj233@gmail.com>
2026-08-10 10:06:01 -0500

    Reconcile ESPN ownership migration

--- drizzle/0004_overrated_colleen_wing.sql
+++ drizzle/0004_overrated_colleen_wing.sql
@@ -1,3 +1,2 @@
-ALTER TABLE `managed_leagues` ADD `roster_id` text;--> statement-breakpoint
 ALTER TABLE `managed_leagues` ADD `league_name` text;--> statement-breakpoint
 ALTER TABLE `managed_leagues` ADD `season` text;
