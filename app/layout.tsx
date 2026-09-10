76c0fbcd760b03d4d14f5ed4e3da5819fdc21e2b
Jordan Jackson <treyj233@gmail.com>
2026-09-10 18:50:44 -0500

    Revert "Redesign Mission Hub as a themed personal briefing home"
    This reverts commit 07d9152effdba5ea3f2bfb4d63a60dd942f18f17.

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,6 @@ import { headers } from "next/headers";
 import { ClerkProvider } from "@clerk/nextjs";
 import "./globals.css";
 import "./readability.css";
-import "./mission-home.css";
 import { getClerkRuntimeKeys } from "./clerk-config";
 
 const geistSans = Geist({ variable: "--font-geist-sans", subsets: ["latin"] });
